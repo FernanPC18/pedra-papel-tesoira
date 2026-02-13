bfddda7a471ce87aaf60da08b87dd0ba8dedb777
GabrielRodriguezGil <gabrielrodgil088@gmail.com>
2026-02-13 14:08:40 +0100

    feat(rpsls_simple): :sparkles: Crear el programa de juego de rpsls, que pasa los test

---
+++ src/rpsls_simple.py
@@ -0,0 +1,91 @@
+import random
+from enum import IntEnum
+
+
+class GameAction(IntEnum):
+    Rock = 0
+    Paper = 1
+    Scissors = 2
+    Spock = 3
+    Lizard = 4
+
+
+class GameResult(IntEnum):
+    Victory = 0
+    Defeat = 1
+    Tie = 2
+
+
+Victories = {
+    GameAction.Rock: [GameAction.Paper, GameAction.Spock],
+    GameAction.Paper: [GameAction.Scissors, GameAction.Lizard],
+    GameAction.Scissors: [GameAction.Rock, GameAction.Spock],
+    GameAction.Spock: [GameAction.Lizard, GameAction.Paper],
+    GameAction.Lizard: [GameAction.Rock, GameAction.Scissors],
+}
+
+
+class Game:
+    def __init__(self):
+        pass
+
+    def assess_game(self, user_action, computer_action):
+
+        game_result = None
+
+        if user_action == computer_action:
+            print(f"User and computer picked {user_action.name}. Draw game!")
+            game_result = GameResult.Tie
+        elif computer_action in Victories[user_action]:
+            print(
+                f"User and computer picked {user_action.name}. You lost, skill issue!"
+            )
+            game_result = GameResult.Defeat
+        else:
+            print(f"User and computer picked {user_action.name}. Win game!")
+            game_result = GameResult.Victory
+
+        return game_result
+
+    def get_computer_action():
+        computer_selection = random.randint(0, len(GameAction) - 1)
+        computer_action = GameAction(computer_selection)
+        print(f"Computer picked {computer_action.name}.")
+
+        return computer_action
+
+    def get_user_action():
+        # Scalable to more options (beyond rock, paper and scissors...)
+        game_choices = [
+            f"{game_action.name}[{game_action.value}]" for game_action in GameAction
+        ]
+        game_choices_str = ", ".join(game_choices)
+        user_selection = int(input(f"\nPick a choice ({game_choices_str}): "))
+        user_action = GameAction(user_selection)
+
+        return user_action
+
+    def play_another_round():
+        another_round = input("\nAnother round? (y/n): ")
+        return another_round.lower() == "y"
+
+
+def main():
+
+    while True:
+        try:
+            user_action = Game.get_user_action()
+        except ValueError:
+            range_str = f"[0, {len(GameAction) - 1}]"
+            print(f"Invalid selection. Pick a choice in range {range_str}!")
+            continue
+
+        computer_action = Game.get_computer_action()
+        Game.assess_game(user_action, computer_action)
+
+        if not Game.play_another_round():
+            break
+
+
+if __name__ == "__main__":
+    Game.assess_game(user_action=GameAction.Paper, computer_action=GameAction.Scissors)
